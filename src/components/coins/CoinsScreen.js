156e8aa80a5be2ac8553d9f655321c4c15cee0d4
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-11-04 21:57:43 -0600

    feat: add pressable and passing params to CoinsItem

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,64 +1,60 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList, Pressable, ActivityIndicator, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {View, FlatList, ActivityIndicator, StyleSheet} from 'react-native';
 import CoinsItem from './CoinsItem';
 import Http from '../../libs/http';
+import Colors from '../../res/colors';
 
-class CoinsScreen extends Component
-{
-    state = {
-        coins: [],
-        loading: false,
-    }
+class CoinsScreen extends Component {
+  state = {
+    coins: [],
+    loading: false,
+  };
 
-    componentDidMount = async () => {
-        this.setState({ loading: true });
+  componentDidMount = async () => {
+    this.setState({loading: true});
 
-        const res = await Http.instance.get('https://api.coinlore.net/api/tickers/');
+    const res = await Http.instance.get(
+      'https://api.coinlore.net/api/tickers/',
+    );
 
-        this.setState({
-            coins: res.data,
-            loading: false,
-        });
-    }
+    this.setState({
+      coins: res.data,
+      loading: false,
+    });
+  };
 
-    handlePress = () => {
-        console.log('Go to detail', this.props);
+  handlePress = (coin) => {
+    this.props.navigation.navigate('CoinDetail', {coin});
+  };
 
-        this.props.navigation.navigate('CoinDetail');
-    }
+  render() {
+    const {coins, loading} = this.state;
 
-    render () {
-        const { coins, loading } = this.state;
+    return (
+      <View style={styles.container}>
+        {loading ? (
+          <ActivityIndicator style={styles.loader} color="#FFF" size="large" />
+        ) : null}
 
-        return (
-            <View 
-                style={styles.container}
-            >
-                { loading 
-                    ? <ActivityIndicator 
-                        style={styles.loader}
-                        color="#FFF" 
-                        size="large" 
-                    />
-                    : null
-                }
-
-                <FlatList 
-                    data={coins}
-                    renderItem={({ item }) => <CoinsItem item={ item } />} 
-                />
-            </View>
-        );
-    }
+        <FlatList
+          data={coins}
+          renderItem={({item}) => (
+            <CoinsItem item={item} onPress={() => this.handlePress(item)} />
+          )}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    loader: {
-        marginTop: 60
-    }
+  container: {
+    flex: 1,
+    backgroundColor: Colors.charade,
+  },
+  loader: {
+    marginTop: 60,
+  },
 });
 
 export default CoinsScreen;
